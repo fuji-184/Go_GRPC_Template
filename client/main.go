a84596b325c742e9bfd4c45e0ef7be2d15d81cef
Fuji <fujisantoso134@gmail.com>
2025-04-13 21:17:15 +0700

    added tls

--- client/main.go
+++ client/main.go
@@ -4,13 +4,18 @@ import (
     "log"
 
     "google.golang.org/grpc"
-    "google.golang.org/grpc/credentials/insecure"
+    "google.golang.org/grpc/credentials"
 
     "github.com/fuji-184/streaming/client_impls"
 )
 
 func main(){
-    conn, err := grpc.Dial("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+    creds, err := credentials.NewClientTLSFromFile("tls/server.crt", "")
+    if err != nil {
+        log.Fatalf("failed to load tls files for client: %v", err)
+    }
+
+    conn, err := grpc.Dial("localhost:50051", grpc.WithTransportCredentials(creds))
     if err != nil {
         log.Fatalf("failed to connect: %v", err)
     }
--- server/main.go
+++ server/main.go
@@ -6,16 +6,23 @@ import (
     "net"
 
     "google.golang.org/grpc"
+    "google.golang.org/grpc/credentials"
     "github.com/fuji-184/streaming/server_impls"
 )
 
 func main(){
+    creds, err := credentials.NewServerTLSFromFile("tls/server.crt", "tls/server.key")
+    if err != nil {
+        log.Fatalf("failed to load tls files for server: %v", err)
+    }
+
     listener, err := net.Listen("tcp", ":50051")
     if err != nil {
         log.Fatalf("failed to listen: %v", err)
     }
 
     s := grpc.NewServer(
+        grpc.Creds(creds),
         grpc.UnaryInterceptor(func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
             log.Printf("unary interceptor: %s", info.FullMethod)
             return handler(ctx, req)
